a979306b2ef44f44f4746f48a72c73e98dd769fb
metulburr <metulburr@gmail.com>
2013-07-20 22:23:22 -0400

    added weight mechanics, hardcoded

--- data/weight_tab.py
+++ data/weight_tab.py
@@ -1,4 +1,3 @@
-
 try:
     import tkinter as tk
     import tkinter.messagebox as msgbox
@@ -6,111 +5,15 @@ except ImportError:
     import Tkinter as tk
     import tkMessageBox as msgbox
 
-
-
-class WM:
-    def __init__(self, parent):
+class Weight(window_manager.WB):
+    def __init__(self, parent, widgets, weight, start, goal, cal):
         self.root = parent
-        self.widgets = []
-
-        #test
-        self.food_num = 0
-        self.activity_num = 0
-        self.weight_num = 250
-        self.mood_num = 10
-        self.cal_budget_num = 2000
-        self.net_cal_num = -3000
-        self.start_weight = 260
-        self.goal_weight = 200
-        
-        self.button_layout()
-        self.on_home()
-    
-    def clear(self):
-        for w in self.widgets[:]:
-            w.grid_forget()
-            #w.destroy()
-            self.widgets.remove(w)
-        
-    def button_layout(self):
-        tk.Button(self.root, height=2, width=10, text="Home", command=lambda:self.on_home()).grid(
-            row=0, column=0)
-        tk.Button(self.root, height=2, width=10, text="Food", command=lambda:self.on_food()).grid(
-            row=0, column=1)
-        tk.Button(self.root, height=2, width=10, text="Activity", command=lambda:self.on_activity()).grid(
-            row=0, column=2)
-        tk.Button(self.root, height=2, width=10, text="Weight", command=lambda:self.on_weight()).grid(
-            row=0, column=3)
-        tk.Button(self.root, height=2, width=10, text="Mood", command=lambda:self.on_mood()).grid(
-            row=0, column=4)
-        tk.Button(self.root, height=2, width=10, text="Calorie\nBudget", command=lambda:self.on_cal_budget()).grid(
-            row=0, column=5)
-        tk.Button(self.root, height=2, width=10, text="Net\nCalories", command=lambda:self.on_net_cal()).grid(
-            row=0, column=6)
-        tk.Button(self.root, height=2, width=10, text="Graph", command=lambda:self.on_graph()).grid(
-            row=0, column=7)
-            
-    def on_home(self):
-        self.clear()
-        self.root.title('Home')
-        
-        
-        #display all values for corresponding section
-        t = tk.Label(self.root, text=self.food_num)
-        self.widgets.append(t)
-        t.grid(row=1, column=1)
-        
-        t = tk.Label(self.root, text=self.activity_num)
-        self.widgets.append(t)
-        t.grid(row=1, column=2)
-
-        t = tk.Label(self.root, text=self.weight_num)
-        self.widgets.append(t)
-        t.grid(row=1, column=3)
-        
-        t = tk.Label(self.root, text=self.mood_num)
-        self.widgets.append(t)
-        t.grid(row=1, column=4)
-        
-        t = tk.Label(self.root, text=self.cal_budget_num)
-        self.widgets.append(t)
-        t.grid(row=1, column=5)
-        
-        t = tk.Label(self.root, text=self.net_cal_num)
-        self.widgets.append(t)
-        t.grid(row=1, column=6)
-        
-        
-
-        
-    def on_food(self):
-        self.clear()
-        self.root.title('Food')
-        
-        t = tk.Label(self.root, text='inside of food')
-        self.widgets.append(t)
-        t.grid(row=1, column=0)
-        
-
-       
-        
-        
-    def on_activity(self):
-        self.clear()
-        self.root.title('Activity')
-
-        t = tk.Label(self.root, text='inside of Activity')
-        self.widgets.append(t)
-        t.grid(row=1, column=0)
-        
-    def on_weight(self):
-        self.clear()
-        self.root.title('Weight')
-        
-        #weight = weight_tab.Weight(self.root, self.widgets, self.weight_num, self.start_weight, self.goal_weight, self.cal_budget_num)
-        #self.widgets = weight.widgets
-        #self.weight_num = weight.weight_num
-        
+        self.widgets = widgets
+        self.weight_num = weight
+        self.start_weight = start
+        self.goal_weight = goal
+        self.cal_budget_num = cal
+        #pass 
         #weight
         weight_text = 'Current Weight\n{}'.format(str(self.weight_num))
         weight_lbl = tk.Label(self.root, text=weight_text)
@@ -227,40 +130,3 @@ class WM:
                 else:
                     var.set(self.cal_budget_num)
         
-
-        
-        
-        
-        
-    def on_mood(self):
-        self.clear()
-        self.root.title('Mood')
-        
-        t = tk.Label(self.root, text='inside of mood')
-        self.widgets.append(t)
-        t.grid(row=1, column=0)
-        
-    def on_cal_budget(self):
-        self.clear()
-        self.root.title('Calorie Budget')
-        
-        t = tk.Label(self.root, text='inside of calbud')
-        self.widgets.append(t)
-        t.grid(row=1, column=0)
-        
-    def on_net_cal(self):
-        self.clear()
-        self.root.title('Net Calories')
-        
-        t = tk.Label(self.root, text='inside of netcal')
-        self.widgets.append(t)
-        t.grid(row=1, column=0)
-       
-        
-    def on_graph(self):
-        self.clear()
-        self.root.title('Graph')
-        
-        t = tk.Label(self.root, text='inside of gragh')
-        self.widgets.append(t)
-        t.grid(row=1, column=0)
